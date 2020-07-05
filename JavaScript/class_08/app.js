dad8a5ddbe5b467ad35865b5b12f83180c742806
HINA KHADIM <hinakhadim2002@gmail.com>
2020-07-05 22:12:25 +0500

    dom nodetypes, nodevalues, childNode

--- JavaScript/class_08/app.js
+++ JavaScript/class_08/app.js
@@ -13,48 +13,50 @@
 //   console.log("setTimeout");
 // }, 60000);
 
-var min = 0;
-var sec = 0;
-var msec = 0;
-var minHeading = document.getElementById("min");
-var secHeading = document.getElementById("sec");
-var msecHeading = document.getElementById("msec");
+// var min = 0;
+// var sec = 0;
+// var msec = 0;
+// var minHeading = document.getElementById("min");
+// var secHeading = document.getElementById("sec");
+// var msecHeading = document.getElementById("msec");
 
-var interval;
+// var interval;
 
-function timer() {
-  msec++;
-  msecHeading.innerHTML = msec;
+// function timer() {
+//   msec++;
+//   msecHeading.innerHTML = msec;
 
-  if (msec >= 100) {
-    sec++;
-    secHeading.innerHTML = sec;
-    msec = 0;
-  } else if (sec >= 60) {
-    min++;
-    sec = 0;
-    minHeading.innerHTML = min;
-  }
-}
+//   if (msec >= 100) {
+//     sec++;
+//     secHeading.innerHTML = sec;
+//     msec = 0;
+//   } else if (sec >= 60) {
+//     min++;
+//     sec = 0;
+//     minHeading.innerHTML = min;
+//   }
+// }
 
-function start() {
-  interval = setInterval(timer, 10);
-  document.getElementById("start").disabled = true;
-}
+// function start() {
+//   interval = setInterval(timer, 10);
+//   document.getElementById("start").disabled = true;
+// }
 
-function stop() {
-  console.log("stopped");
-  clearInterval(interval);
-  document.getElementById("start").disabled = false;
-}
+// function stop() {
+//   console.log("stopped");
+//   clearInterval(interval);
+//   document.getElementById("start").disabled = false;
+// }
 
-function reset() {
-  stop();
-  min = 0;
-  msec = 0;
-  sec = 0;
-  minHeading.innerHTML = min;
-  secHeading.innerHTML = sec;
-  msecHeading.innerHTML = msec;
-  document.getElementById("start").disabled = false;
-}
+// function reset() {
+//   stop();
+//   min = 0;
+//   msec = 0;
+//   sec = 0;
+//   minHeading.innerHTML = min;
+//   secHeading.innerHTML = sec;
+//   msecHeading.innerHTML = msec;
+//   document.getElementById("start").disabled = false;
+// }
+
+console.log(document.childNodes[1].childNodes[1]);
