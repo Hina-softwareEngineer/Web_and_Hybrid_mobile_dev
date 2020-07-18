8cf38843388faac8fac5b01a3de2990aea34cfb1
HINA KHADIM <hinakhadim2002@gmail.com>
2020-07-19 00:17:19 +0500

    39-67

--- Javascript_Assignment_39-67/app.js
+++ Javascript_Assignment_39-67/app.js
@@ -1,4 +1,4 @@
-function power(a, b) {
+/*function power(a, b) {
   return Math.pow(a, b);
 }
 
@@ -163,3 +163,138 @@ document.querySelector(".dec").addEventListener("click", function () {
   count -= 1;
   h2.innerText = count;
 });
+*/
+
+/*
+let array = [];
+let obj = {};
+obj.firstName = "John";
+obj.lastName = "Doe";
+obj.age = 90;
+
+if (true) {
+  obj.hobby = "table tennis";
+  console.log(obj.hobby);
+  array.push(obj);
+  console.log(array);
+  console.log("1st trye");
+}
+
+if (true) {
+  obj.hobby = "marshal arts";
+  console.log(obj.hobby);
+  array.push(obj);
+  console.log(array);
+  console.log("2nd arrat");
+}
+
+// 49-52
+
+let submit = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+let ol = document.createElement("ol");
+submit.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let li = document.createElement("li");
+  let li1 = document.createElement("li");
+  let li2 = document.createElement("li");
+  li.innerText = inputs[0].value;
+  ol.appendChild(li);
+  li1.innerText = inputs[1].value;
+  ol.appendChild(li1);
+  li2.innerText = inputs[2].value;
+  ol.appendChild(li2);
+
+  document.body.appendChild(ol);
+});
+
+let more = document.getElementById("more");
+let read = document.getElementById("read");
+
+more.style.display = "none";
+
+read.addEventListener("click", function () {
+  more.style.display = "block";
+});
+
+var name = "hina";
+var classes = 8;
+for (var i = 0; i < 4; i++) {
+  let row = document.createElement("tr");
+  row.id = "row" + i;
+  let text = `<td>${i}</td><td>${name + i}</td><td>${
+    classes + i
+  }</td><td><button id="edit">Edit</button></td><button id="buttons">Delete</button></td>`;
+  row.innerHTML = text;
+  document.querySelector("table").appendChild(row);
+}
+
+let button = document.querySelectorAll("#buttons");
+
+for (var i = 0; i < 4; i++) {
+  button[i].addEventListener("click", function (e) {
+    var r = document.querySelector(`#${e.target.parentElement.id}`);
+    console.log(e.target.parentElement.id);
+    // r.remove();
+  });
+}
+
+let hide = document.querySelector("#hidden");
+hide.style.display = "none";
+let edit = document.querySelectorAll("#edit");
+for (var i = 0; i < 4; i++) {
+  edit[i].addEventListener("click", function (e) {
+    var r = document.querySelector(
+      `#${e.target.parentElement.parentElement.id}`
+    );
+    let td = r;
+    let childs = td.childNodes;
+    let id = childs[0].textContent;
+    let name = childs[1].textContent;
+    let classes = childs[2].textContent;
+
+    let inputShowing = hide.childNodes;
+    inputShowing[1].value = id;
+    inputShowing[3].value = name;
+    inputShowing[5].value = classes;
+
+    hide.style.display = "block";
+  });
+}
+*/
+
+// 58-67
+
+let main_content = document.getElementById("main-content");
+
+//console.log(main_content.childNodes);
+
+let render = document.querySelectorAll(".render");
+for (var i = 0; i < render.length; i++) {
+  console.log(render[i].innerHTML);
+}
+
+let full_name, last_name, email;
+full_name = document.querySelector("#first-name");
+last_name = document.querySelector("#last-name");
+email = document.querySelector(".email");
+
+full_name.value = "Hina";
+last_name.value = "Khadim";
+email.value = "email@example.com";
+
+//----------
+
+let form_content = document.querySelector(".form-content");
+// div type
+console.log(form_content.nodeType);
+let lastName = document.querySelector("#lastName");
+console.log(lastName.nodeType, lastName.childNodes);
+
+// by inserting a child node
+
+console.log(main_content.firstChild, main_content.lastChild);
+console.log(lastName.previousSibling, lastName.nextSibling);
+
+console.log(email.parentNode, email.nodeType);
